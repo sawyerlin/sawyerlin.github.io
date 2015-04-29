456363fae7e852225493f6fa97f4fdf4f47814a1
sawyerlin <sawyer.lin@gmail.com>
2015-04-29 18:45:07 +0200

    add angular route

---
+++ app/js/app.js
@@ -0,0 +1,30 @@
+(function(angular){ 
+    'use strict';
+
+    var sawyerApp = angular.module('sawyerApp', [
+            'ngRoute',
+            'sawyerControllers'
+        ]);
+
+    sawyerApp.config(['$routeProvider', function($routeProvider) {
+            $routeProvider
+                .when('/home', {
+                    templateUrl: 'templates/home.html',
+                    controller: 'HomeController'
+                })
+                .when('/demos', {
+                    templateUrl: 'templates/demos.html',
+                    controller: 'DemosController'
+                })
+                .when('/experiments', {
+                    templateUrl: 'templates/experiments.html',
+                    controller: 'ExperimentsController'
+                })
+                .when('/contact', {
+                    templateUrl: 'templates/contact.html',
+                    controller: 'ContactController'
+                });
+        }
+    ]);
+
+})(angular);
