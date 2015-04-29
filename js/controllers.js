2dc01116d0019b798750debe118e760fe2cdc2e4
sawyerlin <sawyer.lin@gmail.com>
2015-04-29 18:45:07 +0200

    add angular route

---
+++ js/controllers.js
@@ -0,0 +1,26 @@
+(function(angular) {
+    'use strict';
+
+    var sawyerControllers = angular.module('sawyerControllers', []);
+
+
+    sawyerControllers.controller('HomeController', ['$scope', '$routeParams',
+            function($scope, $routeParams) {
+            }]
+    );
+
+    sawyerControllers.controller('DemosController', ['$scope', '$routeParams',
+            function($scope, $routeParams) {
+            }]
+    );
+
+    sawyerControllers.controller('ExperimentsController', ['$scope', '$routeParams',
+            function($scope, $routeParams) {
+            }]
+    );
+
+    sawyerControllers.controller('ContactController', ['$scope', '$routeParams',
+            function($scope, $routeParams) {
+            }]
+    );
+})(angular);
