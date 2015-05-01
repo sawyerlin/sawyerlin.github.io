677f02ec5f2f66b7b26ce94076afcc5f606f137b
sawyerlin <sawyer.lin@gmail.com>
2015-05-01 14:37:17 +0200

    add active class for header items

---
+++ app/js/config.js
@@ -0,0 +1,32 @@
+var config = {
+    headers: {
+        contents: [
+            {
+                name: 'Home',
+                value: 'home'
+            },
+            {
+                name: 'Todo',
+                value: 'todo',
+                class: 'active'
+            },
+            {
+                name: 'Demos',
+                value: 'demos'
+            },
+            {
+                name: 'Experiments',
+                value: 'experiments'
+            },
+            {
+                name: 'Work',
+                value: 'work'
+            },
+            {
+                name: 'Contact',
+                value: 'contact'
+            }
+        ],
+        active: 'todo' 
+    }
+};
--- app/sources/todo.md
+++ app/sources/todo.md
@@ -1,6 +1,6 @@
 # This is todo list for the site SL
 
 - basic structure (done)
-- focus header links
+- focus header links (done)
 - unify all resources for working in the work page
 - other todo
