e6741ee98a2a050b472e9e430461de84c70ad1ee
sawyerlin <sawyer.lin@gmail.com>
2015-12-09 18:36:13 +0100

    add project page and github api

--- js/controllers.js
+++ js/controllers.js
@@ -2,85 +2,70 @@
     'use strict';
     var sawyerControllers = angular.module('sawyerControllers', []);
     sawyerControllers.controller('HeadController', ['$scope', 
-            function($scope) {
-                $scope.headers = config.headers.contents;
+        function($scope) {
+            $scope.headers = config.headers.contents;
 
-                var headerValue = window.location.hash.slice(2, window.location.hash.length);
-                if (headerValue.length && config.headers.active !== headerValue ) {
-                    var header = $scope.headers[headerValue];
-                    var oldHeader = $scope.headers[config.headers.active];
-                    
-                    if (header) {
-                        header.class = oldHeader.class;
-                        delete oldHeader.class;
-                    }
-                    config.headers.active = headerValue;
+            var headerValue = window.location.hash.slice(2, window.location.hash.length);
+            if (headerValue.length && config.headers.active !== headerValue ) {
+                var header = $scope.headers[headerValue];
+                var oldHeader = $scope.headers[config.headers.active];
+
+                if (header) {
+                    header.class = oldHeader.class;
+                    delete oldHeader.class;
+                }
+                config.headers.active = headerValue;
+            }
+
+            $scope.changeHeader = function(headerValue) {
+                if (config.headers.active === headerValue) return;
+                if (config.headers.active) {
+                    $('#' + config.headers.active).removeClass('active');
                 }
 
-                $scope.changeHeader = function(headerValue) {
-                    if (config.headers.active === headerValue) return;
-                    if (config.headers.active) {
-                        $('#' + config.headers.active).removeClass('active');
-                    }
-
-                    $('#' + headerValue).addClass('active');
-                    config.headers.active = headerValue;
-                };
-            }]
+                $('#' + headerValue).addClass('active');
+                config.headers.active = headerValue;
+            };
+        }]
     );
 
     sawyerControllers.controller('AboutController', ['$scope', '$routeParams', '$http',
-            function($scope, $routeParams, $http) {
-                $http.get('sources/about.md').success(function(data) {
-                    $scope.markdown = data;
-                });
-            }]
-    );
-
-    sawyerControllers.controller('ToolsController', ['$scope', '$routeParams',
-            function($scope, $routeParams) {
-                $scope.headers = config.headers.contents[config.headers.active].contents;
-                $scope.changeHeader = function(headerValue) {
-
-                };
-            }]
-    );
-
-    sawyerControllers.controller('DemosController', ['$scope', '$routeParams',
-            function($scope, $routeParams) {
-            }]
-    );
-
-    sawyerControllers.controller('ExperimentsController', ['$scope', '$routeParams',
-            function($scope, $routeParams) {
-            }]
-    );
-
-    sawyerControllers.controller('ContactController', ['$scope', '$routeParams',
-            function($scope, $routeParams) {
-            }]
-    );
-
-    sawyerControllers.controller('WorkController', ['$scope', '$routeParams',
-            function($scope, $routeParams) {
-            }]
+        function($scope, $routeParams, $http) {
+            $http.get('sources/about.md').success(function(data) {
+                $scope.markdown = data;
+            });
+        }]
     );
 
     sawyerControllers.controller('TodoController', ['$scope', '$routeParams', '$http',
-            function($scope, $routeParams, $http) {
-                $http.get('sources/todo.md').success(function(data) {
-                    $scope.markdown = data;
+        function($scope, $routeParams, $http) {
+            $http.get('sources/todo.md').success(function(data) {
+                $scope.markdown = data;
+            });
+        }]
+    );
+
+    sawyerControllers.controller('ProjectController', ['$scope', '$routeParams', '$http',
+        function($scope, $routeParams, $http) {
+            console.log('project controller');
+            $scope.loadProfile = function(profileName) {
+                $http.get('https://api.github.com/users/' + profileName).success(function(data) {
+                    $scope.profile = data;
                 });
-            }]
+            };
+            $scope.cleanProfile = function() {
+                $scope.profile = null;
+            };
+        }]
     );
 
     sawyerControllers.controller('RecordController', ['$scope', '$routeParams', '$http', 
-            function($scope, $routeParams, $http) {
-                var name = $routeParams.name || 'index.md';
-                $http.get('sources/record/' + name).success(function(data) {
-                    $scope.markdown = data;
-                });
-            }]
+        function($scope, $routeParams, $http) {
+            var name = $routeParams.name || 'index.md';
+            $http.get('sources/record/' + name).success(function(data) {
+                $scope.markdown = data;
+            });
+        }]
     );
 
 })(window.angular);
