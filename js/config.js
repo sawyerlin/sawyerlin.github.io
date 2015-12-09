417925fefeed6baaf69a58694980cf36b698623b
sawyerlin <sawyer.lin@gmail.com>
2015-12-09 17:12:56 +0100

    add cross browser support

--- js/config.js
+++ js/config.js
@@ -1,37 +1,37 @@
 var config = {
     headers: {
         contents: {
-            home: {
-                name: 'Home'
+            about: {
+                name: 'About'
             },
-            tools: {
-                name: 'Tools',
-                contents: {
-                    compare : {name: 'Compare', class: 'active'},
-                    regex: {name: 'Regular Expression'},
-                    base64: {name: 'Base 64'}
-                }
-            },
+            /*tools: {*/
+                //name: 'Tools',
+                //contents: {
+                    //compare : {name: 'Compare', class: 'active'},
+                    //regex: {name: 'Regular Expression'},
+                    //base64: {name: 'Base 64'}
+                //}
+            /*},*/
             todo: {
                 name: 'Todo'
             },
-            demos: {
-                name: 'Demos'
-            },
-            experiments: {
-                name: 'Experiments'
-            },
-            work: {
-                name: 'Work'
-            },
-            contact: {
-                name: 'Contact'
-            },
+            /*demos: {*/
+                //name: 'Demos'
+            //},
+            //experiments: {
+                //name: 'Experiments'
+            //},
+            //work: {
+                //name: 'Work'
+            //},
+            //contact: {
+                //name: 'Contact'
+            /*},*/
             record: {
                 name: 'Record',
                 class: 'active'
             }
         },
-        active: 'work'
+        active: 'record'
     }
 };
