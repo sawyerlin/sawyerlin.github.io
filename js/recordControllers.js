e76e513d842703cfc8ad132f904cc55af2aac888
sawyerlin <sawyer.lin@gmail.com>
2016-06-10 14:47:20 +0200

    add DISQUS

--- js/recordControllers.js
+++ js/recordControllers.js
@@ -3,12 +3,21 @@
     var recordControllers = angular.module('recordControllers', []);
 
     recordControllers.controller('RecordController', ['$scope', '$routeParams', '$http',
-        function($scope, $routeParams, $http) {
-            var name = $routeParams.name || 'index.md';
-            $http.get('../sources/record/' + name).success(function(data) {
-                $scope.markdown = data;
-            });
-        }]
-    );
+            function($scope, $routeParams, $http) {
+                var name = $routeParams.name || 'index.md';
+                $http.get('../sources/record/' + name).success(function(data) {
+                    $scope.markdown = data;
+                    if (name !== 'index.md') {
+                        DISQUS.reset({
+                            reload: true,
+                            config: function () {
+                                this.page.url = window.localtion.href;
+                                this.page.identifier = name;
+                            }
+                        });
+                    }
+                });
+            }]
+            );
 
 })(window.angular);
